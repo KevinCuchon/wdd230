60f89f030d3ebbf0c9c078191f50f835616cba54
0nly420 <131375398+0nly420@users.noreply.github.com>
2024-03-29 00:47:51 -0500

    Change With Images

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -7,3 +7,19 @@ currentYearElem.textContent = new Date().getFullYear();
 var lastModifiedElem = document.getElementById('lastModified');
 // Asignar la fecha de última modificación al elemento
 lastModifiedElem.textContent = "Last modified: " + document.lastModified;
+
+
+    document.addEventListener('DOMContentLoaded', function() {
+        // Obtiene el número total de visitas almacenado en localStorage
+        let visitCount = localStorage.getItem('visitCount') || 0;
+        
+        // Incrementa el contador de visitas
+        visitCount++;
+        
+        // Almacena el nuevo número total de visitas en localStorage
+        localStorage.setItem('visitCount', visitCount);
+        
+        // Muestra el número total de visitas en la página
+        document.getElementById('visit-counter').textContent = visitCount;
+    });
+
